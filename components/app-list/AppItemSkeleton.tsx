9f2ab196ea05f208ac8a95a1211518456375f29b
Daniel Peña <danielpena@danielpb.local>
2023-02-08 19:30:00 -0400

    organice items skeleton

--- components/app-list/AppItemSkeleton.tsx
+++ components/app-list/AppItemSkeleton.tsx
@@ -25,54 +25,9 @@ const AppItemSkeleton: FC = () => {
 const AppItemsSkeleton: FC = () => {
   return (
     <div className="flex flex-wrap gap-12">
-      <Card>
-        <div className="w-full py-14">
-          <div className="animate-pulse flex space-x-4">
-            <div className="flex-1 space-y-6 py-1">
-              <div className="h-2 bg-gray-300 rounded"></div>
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-gray-300 rounded col-span-2"></div>
-                  <div className="h-2 bg-gray-300 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-gray-300 rounded"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Card>
-      <Card>
-        <div className="w-full py-14">
-          <div className="animate-pulse flex space-x-4">
-            <div className="flex-1 space-y-6 py-1">
-              <div className="h-2 bg-gray-300 rounded"></div>
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-gray-300 rounded col-span-2"></div>
-                  <div className="h-2 bg-gray-300 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-gray-300 rounded"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Card>
-      <Card>
-        <div className="w-full py-14">
-          <div className="animate-pulse flex space-x-4">
-            <div className="flex-1 space-y-6 py-1">
-              <div className="h-2 bg-gray-300 rounded"></div>
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-gray-300 rounded col-span-2"></div>
-                  <div className="h-2 bg-gray-300 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-gray-300 rounded"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Card>
+      <AppItemSkeleton />
+      <AppItemSkeleton />
+      <AppItemSkeleton />
     </div>
   );
 };
